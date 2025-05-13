12b34d72cd596d75c9ebb6cc537a57f796d52a7b
Q1111122222 <igiranezauhiriweguerschom@gmail.com>
2025-05-13 22:24:20 +0300

    Added the Details of the items in the Frontend. : Cabbage :

--- src/components/pages/Details.jsx
+++ src/components/pages/Details.jsx
@@ -1,23 +1,65 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
+import Profile_pic from '../profile_pic';
 
 const Details = () => {
    const location = useLocation();
    console.log("Location state:", location);
-   const { image, name, short_d } = location.state || {};
+   const { image, name, short_d } = location.state || { image: '', name: '', short_d: '' };
 
    if (!name) {
       return <div className="text-center text-red-500">No seed data available.</div>;
    }
 
    return (
-    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <div className="w-2/3 bg-white p-8 rounded-lg shadow-lg">
-        <img src={image} alt={name} className="w-full h-96 object-cover rounded-md mb-6" />
-        <h1 className="text-3xl font-bold mb-2">{name}</h1>
-        <p className="text-gray-700 text-lg">{short_d}</p>
+      <div className="w-full min-h-screen flex flex-col items-center justify-center bg-gray-100">
+         <div className="w-10/12 h-10/12 bg-white p-8 rounded-lg shadow-lg flex items-center justify-evenly">
+            <div className='w-2/5 h-4/5 rounded-xl'>
+               <img src={image} alt="" className='w-full h-full object-cover rounded-xl' />
+            </div>
+            <div className='w-3/6 h-3/4 flex flex-col items-center justify-evenly'>
+               <div className='w-full h-[3.5em] flex items-center'>
+                  <p className='text-4xl font-semibold'>{name}</p>
+               </div>
+               <div className='w-full h-[2em] flex items-center relative -top-[1em]'>
+                  <div className='w-[2em] h-[2em]'>
+                     <Profile_pic />
+                  </div>
+                  <p className='relative left-3 font-medium italic'>Niyomwungeri Felicien</p>
+               </div>
+               {/* Counting button and  */}
+               <div className='w-full h-[4em] flex items-center relative -top-[1em]'>
+                  <div className='w-1/3 h-9/11 rounded-full flex items-center justify-center overflow-hidden ring-1 ring-gray-600'>
+                     <div className='w-1/3 h-full bg-gray-400 flex items-center justify-center ring-1 ring-r-gray-400'> 
+                        <p className='text-4xl font-medium'>-</p>
+                      </div>
+                      {/* Counting the users booking items */}
+                     <div className='w-1/3'></div>
+                     <div className='w-1/3 h-full bg-gray-400 flex items-center justify-center ring-1 ring-l-gray-400'>
+                         <p className='text-4xl font-medium'>+</p>
+                      </div>
+                  </div>
+               </div>
+               {/* Booking and chatting buttons are here */}
+               <div className='w-full h-[5em] flex items-center justify-evenly'>
+                  {/* Booking One */}
+                  <div className='w-5/11 h-8/11 bg-green-600 flex items-center justify-center hover:ring-2 hover:bg-green-600/90 text-2xl font-semibold text-white'>
+                     Book Now
+                  </div>
+                  {/* Chatting One */}
+                  <div className='w-5/11 h-8/11 ring-2 ring-green-600 flex items-center justify-center text-2xl font-semibold text-green-600 hover:bg-green-600 hover:ring-0 hover:text-white'>
+                     Chat Now
+                  </div>
+               </div>
+               {/* Descriptions */}
+               <hr className='w-full text-gray-600 font-bold' />
+               <div className='w-full h-[7em]'>
+                  <p className='relative -top-[1em] text-lg text-gray-600 font-medium left-[.9em]'>Descriptions</p>
+                  <p className='h-1/2 text-gray-600 leading-[1.3em] left-[.9em]'>{short_d}</p>
+               </div>
+            </div>
+         </div>
       </div>
-    </div>
    )
 }
 
--- src/components/pages/items/Card.jsx
+++ src/components/pages/items/Card.jsx
@@ -1,47 +1,80 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom"
-import Button from '../../Button'
-import { Link } from "react-router-dom"
+// import React from 'react'
+// import { useNavigate } from "react-router-dom"
+// import Button from '../../Button'
+// import { Link } from "react-router-dom"
 
 
 
-const Card = ({ image, name, short_d}) => {
+// const Card = ({ image, name, short_d}) => {
 
-   const navigate = useNavigate();
+//    const navigate = useNavigate();
 
-   const seedDetail = () => {
-     navigate('/seedDetails', {
-      state: {
-         image,
-         name,
-         short_d
-      }
-     })
-   }
+//    const seedDetail = () => {
+//       console.log("Navigating with:", { image, name, short_d });
+//       navigate('/seedDetails', {
+//         state: {
+//           image,
+//           name,
+//           short_d
+//         }
+//       });
+//     }
 
+//   return (
+//     <div className='w-1/5 h-[18em] bg-white rounded-xl flex flex-col items-center justify-evenly shadow-md shadow-black/20'>
+//       <div className='w-11/12 h-3/6'>
+//          <img src={image} alt={name} className='w-full h-full object-cover rounded-t-2xl rounded-b-2xl' />
+//       </div>
+
+//       <div className='w-full h-3/8 flex flex-col items-center justify-evenly'>
+//          <div className='w-9/10 h-1/3'>
+//             <p className='text-lg font-medium relative ml-8'>{ name }</p>
+//          </div>
+//          <div className='w-9/10 h-1/4'>
+//             <p className='text-gray-500/50 text-md font-md relative ml-8'>{short_d}</p>
+//          </div>
+//          <div className='w-full h-[2.5em] flex items-center justify-center'>
+//             <div className='w-9/11 h-12/12 bg-white shadow-sm shadow-black/10 rounded-full flex items-center justify-end'>
+//                <Link to="/seedDetails" className='w-full h-full flex items-center justify-center'>
+//                   <Button onClick={seedDetail}>Detail</Button>
+//                </Link>
+//             </div>
+//          </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Card
+
+
+import React from 'react';
+import { Link } from "react-router-dom";
+import Button from '../../Button';
+
+const Card = ({ image, name, short_d }) => {
   return (
     <div className='w-1/5 h-[18em] bg-white rounded-xl flex flex-col items-center justify-evenly shadow-md shadow-black/20'>
       <div className='w-11/12 h-3/6'>
-         <img src={image} alt={name} className='w-full h-full object-cover rounded-t-2xl rounded-b-2xl' />
+        <img src={image} alt={name} className='w-full h-full object-cover rounded-t-2xl rounded-b-2xl' />
       </div>
-
       <div className='w-full h-3/8 flex flex-col items-center justify-evenly'>
-         <div className='w-9/10 h-1/3'>
-            <p className='text-lg font-medium relative ml-8'>{ name }</p>
-         </div>
-         <div className='w-9/10 h-1/4'>
-            <p className='text-gray-500/50 text-md font-md relative ml-8'>{short_d}</p>
-         </div>
-         <div className='w-full h-[2.5em] flex items-center justify-center'>
-            <div className='w-9/11 h-12/12 bg-white shadow-sm shadow-black/10 rounded-full flex items-center justify-end'>
-               <Link to="/seedDetails" className='w-full h-full flex items-center justify-center'>
-                  <Button onClick={seedDetail}>Detail</Button>
-               </Link>
-            </div>
-         </div>
+        <div className='w-9/10 h-1/3'>
+          <p className='text-lg font-medium relative ml-8'>{name}</p>
+        </div>
+        <div className='w-9/10 h-1/4'>
+          <p className='text-gray-500/50 text-md font-md relative ml-8'>{short_d}</p>
+        </div>
+        <div className='w-full h-[2.5em] flex items-center justify-center'>
+          <div className='w-9/11 h-12/12 bg-white shadow-sm shadow-black/10 rounded-full flex items-center justify-end'>
+            <Link to="/seedDetails" state={{ image, name, short_d }} className='w-full h-full flex items-center justify-center'>
+              <Button>Detail</Button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
